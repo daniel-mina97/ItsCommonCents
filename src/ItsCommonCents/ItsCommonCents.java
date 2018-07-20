4cdf53d103b5950f73223a9cc23d3dbe7ca79bc4
Daniel Mina <minadaniel27@gmail.com>
2018-07-20 17:23:52 -0500

    Change casing of folder name

---
+++ src/ItsCommonCents/ItsCommonCents.java
@@ -0,0 +1,13 @@
+package ItsCommonCents;
+
+import Forms.SettingsInitializer;
+
+public class ItsCommonCents {
+
+    public static void main(String[] args) {
+        
+        SettingsInitializer frame = new SettingsInitializer();
+        frame.setVisible(true);
+    }
+    
+}
