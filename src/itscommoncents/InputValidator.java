c31f012c5a1be36808f4218be8349f829a96f14b
Daniel Mina <minadaniel27@gmail.com>
2018-07-19 22:39:56 -0500

    Test if input can be interpreted as dollars
    Created a method that will take a string and test whether or not it can be converted into a numeric dollar value. It will test that it is an actual number, and also that it has at most 2 decimal places.

--- src/itscommoncents/InputValidator.java
+++ src/itscommoncents/InputValidator.java
@@ -1,6 +1,26 @@
 package itscommoncents;
 
+import java.lang.Character;
 
 public class InputValidator {
+    
+    public static boolean canConvertStringToDollars(String input) {
+        int decimalPlacesCount = 0;
+        boolean decimalPointPassed = false;
+        for(char c : input.toCharArray()) {
+            if(Character.isDigit(c) && decimalPlacesCount < 2) {
+                if(decimalPointPassed) {
+                    decimalPlacesCount++;
+                } 
+            }
+            else if(c == '.' && !decimalPointPassed) {
+                decimalPointPassed = true;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
     
 }
