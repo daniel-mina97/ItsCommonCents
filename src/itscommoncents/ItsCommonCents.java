25248890f97dbecbbdacba018eda3b5363783f7c
Daniel Mina <minadaniel27@gmail.com>
2018-07-19 22:32:53 -0500

    Use real main method
    The main method located in the itscommoncents package will now create an instance of the SettingsInitializer GUI.

--- src/itscommoncents/ItsCommonCents.java
+++ src/itscommoncents/ItsCommonCents.java
@@ -1,8 +1,13 @@
 package itscommoncents;
 
+import Forms.SettingsInitializer;
+
 public class ItsCommonCents {
 
     public static void main(String[] args) {
+        
+        SettingsInitializer frame = new SettingsInitializer();
+        frame.setVisible(true);
     }
     
 }
