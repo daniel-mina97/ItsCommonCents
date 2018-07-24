bd35813ef3e963cb99542a3a1ab072c40c570b68
Daniel Mina <minadaniel27@gmail.com>
2018-07-24 00:07:26 -0500

    Set form close operation

--- src/ItsCommonCents/ItsCommonCents.java
+++ src/ItsCommonCents/ItsCommonCents.java
@@ -1,12 +1,14 @@
 package ItsCommonCents;
 
 import Forms.SettingsInitializer;
+import javax.swing.JFrame;
 
 public class ItsCommonCents {
 
     public static void main(String[] args) {
         
         SettingsInitializer frame = new SettingsInitializer();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
     
