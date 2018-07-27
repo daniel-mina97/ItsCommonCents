99e1379b412795cdc997680b2c0abd4490c34cf1
Daniel Mina <minadaniel27@gmail.com>
2018-07-26 21:06:39 -0500

    Create database object in driver class
    In the driver class of the application, we have instantiated a DatabaseConnection object to provide the connectivity to the database.
    
    Co-Authored-By: Ella Robertson <ellarobertson@users.noreply.github.com>

--- src/ItsCommonCents/ItsCommonCents.java
+++ src/ItsCommonCents/ItsCommonCents.java
@@ -1,11 +1,16 @@
 package ItsCommonCents;
 
+import DataManagement.DatabaseConnection;
 import Forms.SettingsInitializer;
 import javax.swing.JFrame;
 
 public class ItsCommonCents {
 
+    private static DatabaseConnection connection;
+    
     public static void main(String[] args) {
+        
+        connection = new DatabaseConnection();
         
         SettingsInitializer frame = new SettingsInitializer();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
