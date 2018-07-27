71ade593a3859f3fd6d43e0d128b9860e1514ef0
Daniel Mina <minadaniel27@gmail.com>
2018-07-26 21:05:19 -0500

    Create class for Database functionality
    This class will be used by the rest of the application to interact with the SQLite database. It handles all of the details such as creating tables and connecting to the database.
    
    Co-Authored-By: Ella Robertson <ellarobertson@users.noreply.github.com>

---
+++ src/DataManagement/DatabaseConnection.java
@@ -0,0 +1,69 @@
+
+package DataManagement;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    
+    private final static String DBURL = "jdbc:sqlite:CommonCents.db";
+    private final static String DATABASE_FILE_PATH = "CommonCents.db";
+    private final Connection connection;
+    
+    
+    public DatabaseConnection() {
+        System.out.println("Beginning of constructor");
+        connection = connectToDatabase();
+        System.out.println("I connected");
+        if(isDatabaseEmpty()) {
+            createTables();
+        }
+    }
+            
+    private Connection connectToDatabase() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DBURL);
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    private boolean isDatabaseEmpty() {
+        File database = new File(DATABASE_FILE_PATH);
+        return database.exists();
+    }
+    
+    private void createTables() {
+        createExpensesTable();
+        createSpendingLimitsTable();
+    }
+    
+    private void createExpensesTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS expenses ("
+                + " date text,"
+                + " category int,"
+                + " amount_spent real)";
+        executeSQL(sql);
+    }
+    
+    private void createSpendingLimitsTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS spending_limits ("
+                + " category int,"
+                + " spending_limit real)";
+        executeSQL(sql);
+    }
+        
+    private void executeSQL(String query) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
