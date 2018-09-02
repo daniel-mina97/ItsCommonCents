7c0dd190204289c972d0e129d6e1767890700538
Daniel Mina <minadaniel27@gmail.com>
2018-09-01 19:17:32 -0500

    Enum class refactors

--- src/Enums/BudgetCategory.java
+++ src/Enums/BudgetCategory.java
@@ -5,6 +5,8 @@
  */
 package Enums;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Ella
@@ -21,15 +23,45 @@ public class BudgetCategory {
         MISC;
     }
     
-    
-    private final int numVal;
-
-    Category(int numVal) {
-        this.numVal = numVal;
+    private static Category convertIntToCategory(int number) {
+        switch(number) {
+            case 1:
+                return Category.HOUSING;
+            case 2:
+                return Category.UTILITIES;
+            case 3:
+                return Category.GROCERIES;
+            case 4:
+                return Category.ENTERTAINMENT;
+            case 5:
+                return Category.TRANSPORTATION;
+            case 6:
+                return Category.MISC;
+            default:
+                return Category.NONE;
+        }
     }
-
-    public int getNumVal() {
-        return numVal;
+    
+    public static String getBudgetCategoryString(int categoryValue) {
+        Category category = convertIntToCategory(categoryValue);
+        switch(category) {
+            case HOUSING:
+                return "Housing";
+            case UTILITIES:
+                return "Utilities";
+            case GROCERIES:
+                return "Groceries";
+            case ENTERTAINMENT:
+                return "Entertainment";
+            case TRANSPORTATION:
+                return "Transportation";
+            case MISC:
+                return "Miscellaneous";
+            default:
+                return "No Category Chosen";
+        }
+    }
+    
     public static String getBudgetCategoryString(Category category) {
         switch(category) {
             case NONE:
@@ -50,6 +82,7 @@ public class BudgetCategory {
                 return "Invalid Category";
         }
     }
+    
     public static String[] getComboBoxLabels() {
         Category[] categories = Category.values();
         ArrayList<String> labelsArrayList = new ArrayList<>();
@@ -59,5 +92,4 @@ public class BudgetCategory {
         String[] labelsArray = labelsArrayList.toArray(new String[labelsArrayList.size()]);
         return labelsArray;
     }
-   }
 }
